e69042cebb3ccd9391d7a009cedcc8cbc82c3cb0
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-25 10:42:06 +0400

    + doc/
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Makefile
+++ Makefile
@@ -1,2 +1,13 @@
-all: cinth.py
+all: docs/index.html
+
+run: cinth.py 
 	python $^
+
+doc/manual.pdf: doc/Makefile doc/*.tex
+	$(MAKE) -C doc
+
+CWD  = $(CURDIR)
+DOCS = $(CWD)/docs
+docs/index.html: doc/*.tex
+	rm -rf $(DOCS) ; mkdir $(DOCS)
+	cd doc ; latex2html -dir $(DOCS) manual
--- cinth.py
+++ cinth.py
@@ -10,6 +10,9 @@ class Frame:
         return self.dump()
     def dump(self,depth=0,prefix=''):
         tree = self.pad(depth) + self.head(prefix)
+        if not depth: Frame.dumped = []
+        if self in Frame.dumped: return tree + ' _/'
+        else: Frame.dumped.append(self)
         for j in self.nest: tree += j.dump(depth+1)
         return tree
     def pad(self,n):
@@ -33,5 +36,7 @@ class Project(Meta): pass
 prj = Project('ide')
 
 home = Dir('~') ; prj // home
+
+settings = Dir('.settings') ; home // settings
 
 print prj
