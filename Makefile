f475c48b02965f1335e95f4758b7ac50f52624b6
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-25 13:09:07 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ all: doc/manual.pdf
 run: cinth.py 
 	python $^
 
-doc/manual.pdf: doc/Makefile doc/*.tex
+doc/manual.pdf: doc/Makefile doc/*.tex doc/*.py
 	$(MAKE) -C doc
--- doc/frame1.py
+++ doc/frame1.py
@@ -1,6 +1,12 @@
 class Frame:
+    ## construct frame with given name `V`
     def __init__(self,V):
+        # type/class tag
+        # (represented as a string for simplicity)
         self.type = self.__class__.__name__.lower()
+        # scalar value (implementation language type)
         self.val  = V
+        # named slots = attributes = associative array 
         self.slot = {}
+        # nested elements = vector = stack = (de)queue
         self.nest = []
